9f16ccb2389a3f2027b38f6932dd29101a23bfc7
Abubker M <abubker.init@gmail.com>
2022-08-21 18:46:24 +0300

    searching is not implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,190 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaSearch } from "react-icons/fa";
+import FormikList from "./components/FormikList";
+import { Formik, Field, Form, FieldArray } from "formik";
+const categories = [
+  {
+    name: "Bracelets",
+    arr: [
+      { id: 14865, name: "Jasinth Bracelet", cat_name: "Bracelets" },
+      { id: 14867, name: "Inspire Bracelet", cat_name: "Bracelets" },
+      { id: 14870, name: "Jasinthe Bracelet", cat_name: "Bracelets" },
+    ],
+  },
 
+  {
+    name: " ",
+    arr: [
+      { id: 14864, name: "Recurring Item", cat_name: " " },
+      {
+        id: 14868,
+        name: "Recurring Item with questions",
+        cat_name: " ",
+      },
+      {
+        id: 14869,
+        name: "Zero amount item with questions",
+        cat_name: " ",
+      },
+      {
+        id: 14872,
+        name: "Normal item with quesFtions",
+        cat_name: " ",
+      },
+      { id: 14873, name: "normal item", cat_name: " " },
+    ],
+  },
+];
 function App() {
+  let arr = [];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-3xl mx-auto py-2 shadow-md">
+      <Formik
+        initialValues={{
+          taxName: "",
+          categories,
+          applicableItems: [],
+          taxRate: 5,
+          appliedTo: "selectedItems",
+        }}
+        onSubmit={async (values) => {
+          const cartItems = {
+            taxName: values.taxName,
+            applicableItems: values.applicableItems.map((item) => item.id),
+            taxRate: values.taxRate,
+            appliedTo: values.appliedTo,
+          };
+
+          alert(JSON.stringify(cartItems, null, 2));
+        }}
+      >
+        {({ values, setFieldValue }) => (
+          <Form className="">
+            <FieldArray name="applicableItems">
+              {(arrayHelpers) => {
+                return (
+                  <>
+                    <div className="my-8 px-8 max-w-xl">
+                      <h1 className="block text-2xl capitalize">add tax</h1>
+                      <div className="grid grid-cols-3 my-4">
+                        <Field
+                          type={"text"}
+                          placeholder="Tax Name"
+                          name="taxName"
+                          className="block w-full h-10 col-span-2 py-2 px-4 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border border-gray-300 rounded-md"
+                        />
+                        <div className="relative">
+                          <input
+                            name={"taxRate"}
+                            type={"number"}
+                            onChange={(v) => {
+                              setFieldValue("taxRate", v.target.value);
+                            }}
+                            defaultValue={values.taxRate}
+                            placeholder="Tax Rate"
+                            min={0}
+                            className="block w-full h-10 mx-4 py-2 px-4 col-span-1 shadow-sm sm:text-sm focus:ring-gray-500 border border-gray-300 rounded-md"
+                          />
+                          <span className="absolute  text-sm right-0 top-2 -mr-3">
+                            %
+                          </span>
+                        </div>
+                      </div>
+
+                      <div className="flex-col px-4  text-sm">
+                        <div className="flex">
+                          <label htmlFor="allItems" className="text-lg">
+                            <Field
+                              type="radio"
+                              name="appliedTo"
+                              value={"allItems"}
+                              onChange={(v) => {
+                                setFieldValue("appliedTo", v.target.value);
+                                values.categories.forEach((item) =>
+                                  item.arr.map((i) => {
+                                    // arrayHelpers.push(i);
+                                    if (
+                                      !values.applicableItems.some(
+                                        (applicableItem) =>
+                                          applicableItem.id === i.id
+                                      )
+                                    ) {
+                                      arrayHelpers.push(i);
+                                    }
+                                  })
+                                );
+                              }}
+                              className="accent-orange-500  rounded-full w-6 h-6 "
+                            />
+                            <span className="ml-2">
+                              Apply to all items in collection
+                            </span>
+                          </label>
+                        </div>
+                        <div className="flex">
+                          <label htmlFor="selectedItems" className="text-lg">
+                            <Field
+                              type="radio"
+                              name="appliedTo"
+                              value={"selectedItems"}
+                              onChange={(v) => {
+                                setFieldValue("appliedTo", v.target.value);
+                              }}
+                              className="accent-orange-500 rounded-full w-6 h-6 "
+                            />
+                            <span className="ml-2">
+                              Apply to selected items
+                            </span>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="w-full border-t border-gray-200 pt-4">
+                      <div className="px-8 relative flex items-center">
+                        <span className="text-gray-300 p-0.5 my-2 px-2 absolute left-8">
+                          <FaSearch className="h-3 w-3" />
+                        </span>
+                        <Field
+                          type={"text"}
+                          name="search-key"
+                          className="block w-1/2 h-10 col-span-2 py-2 px-6 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border border-gray-300 rounded-md"
+                          placeholder="search for item"
+                        />
+                      </div>
+                    </div>
+                    {values.categories.map((categoryRow, index) => {
+                      // const arr = values.applicableItems.filter(
+                      arr = values.applicableItems.filter(
+                        (category) => category.cat_name === categoryRow.name
+                      );
+
+                      return (
+                        <div key={index} className="mt-4  w-full">
+                          <FormikList
+                            values={values}
+                            arr={arr}
+                            categoryRow={categoryRow}
+                            index={index}
+                            arrayHelpers={arrayHelpers}
+                          />
+                        </div>
+                      );
+                    })}
+                  </>
+                );
+              }}
+            </FieldArray>
+            {/* <FormikList values={values} /> */}
+            <span className="flex my-8 mx-4 justify-end">
+              <button
+                className="px-4 py-4 text-white rounded-sm bg-orange-500 text-lg capitalize"
+                type="submit"
+              >
+                Apply tax to {`${values?.applicableItems?.length}`} item's
+              </button>
+            </span>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
---
+++ src/components/FormikList.jsx
@@ -0,0 +1,95 @@
+import { FieldArray } from "formik";
+
+const parentChecked = (e, categoryRow, values, arrayHelpers) => {
+  if (e.target.checked) {
+    categoryRow.arr.map((item, index) => {
+      if (
+        categoryRow.arr.hasOwnProperty(index) &&
+        !values.applicableItems.some(
+          (applicableItem) => applicableItem.id === item.id
+        )
+      ) {
+        arrayHelpers.push(item);
+      }
+    });
+  } else {
+    categoryRow.arr.map((item, index) => {
+      if (categoryRow.arr.hasOwnProperty(index)) {
+        const idx = values.applicableItems.findIndex(() => {
+          return e.target.name === item.cat_name;
+        });
+        arrayHelpers.remove(idx);
+
+        values.appliedTo = "selectedItems";
+      }
+    });
+  }
+};
+const childChecked = (e, arrayHelpers, values, cateItem) => {
+  if (e.target.checked) {
+    arrayHelpers.push(cateItem);
+  } else {
+    const idx = values.applicableItems.findIndex(
+      (applicableItem) => applicableItem.id === cateItem.id
+    );
+    arrayHelpers.remove(idx);
+    values.appliedTo = "selectedItems";
+  }
+};
+const FormikList = ({ values, arr, index, categoryRow, arrayHelpers }) => {
+  return (
+    <>
+      <label className="text-sm flex  bg-gray-300 flex-row w-full px-3 py-2">
+        <input
+          name={categoryRow.name}
+          type="checkbox"
+          value={index}
+          className="accent-cyan-400"
+          checked={
+            values.appliedTo === "allItems" ||
+            arr.length === categoryRow.arr.length
+              ? true
+              : false
+          }
+          onChange={(e) => {
+            parentChecked(e, categoryRow, values, arrayHelpers);
+          }}
+        />
+        <span className="mx-4 text-base">{categoryRow.name}</span>
+      </label>
+
+      <FieldArray name="applicableItems">
+        {(arrayHelpers) => {
+          return (
+            <div className="text-sm mx-4 flex flex-col w-full px-3 py-2">
+              {categoryRow.arr.map((cateItem) => {
+                return (
+                  <label key={cateItem.id} className="my-2 text-xl">
+                    <input
+                      name="categoryItems"
+                      type="checkbox"
+                      value={cateItem.id}
+                      className="accent-cyan-400"
+                      checked={
+                        values.appliedTo === "allItems" ||
+                        values.applicableItems?.findIndex(
+                          (applicableItem) => applicableItem.id === cateItem.id
+                        ) !== -1
+                      }
+                      onChange={(e) => {
+                        childChecked(e, arrayHelpers, values, cateItem);
+                      }}
+                    />
+                    <span className="mx-2 text-base">{cateItem.name}</span>
+                  </label>
+                );
+              })}
+            </div>
+          );
+        }}
+      </FieldArray>
+    </>
+  );
+};
+
+export default FormikList;
